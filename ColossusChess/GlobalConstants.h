41e666ea3eff85b6b837b91d8f13fb6bbc7e853a
Martin Bryant <martinpaulbryant@hotmail.co.uk>
2026-08-22 15:39:00 +0100

    x

--- ColossusChess/GlobalConstants.h
+++ ColossusChess/GlobalConstants.h
@@ -5,7 +5,7 @@
 //----------------------------------------------------------------------------------------------------
 
 // Version
-#define VersionX "2026a"
+#define VersionX "20NNx5"
 //#define EXPERIMENTAL
 //#define TB_NO_HW_POP_COUNT // Use the same directive name as defined in Ronald de Man's probing code!
 
